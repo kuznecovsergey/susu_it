276543e31e49744be3efaefedabbf1d3a6b53a2b
Antonov Sergey <antonov@mail.com>
2024-11-05 17:32:59 +0500

    Максимальное значение из трех целых чисел

--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 int main() {
     int a = 0;
     int b = 0;
+    int c = 0;
     int max = 0;
 
     printf("Введите число a: ");
     scanf("%d", &a);
     printf("Введите число b: ");
     scanf("%d", &b);
-
-    max = (a > b) ? a : b;
-
+    printf("Введите число с: ");
+    scanf("%d", &c);
+    
+    max = (a > b)   ? a   : b;
+    max = (max > c) ? max : c;
+    
     printf("%d", max);
 
     return 0;
